1f34c4a8f134ee106e07c035abdcde870459e660
dsfsdfsdf <elnurc7@gmail.com>
2021-02-22 14:47:03 +0400

    navdrawer

---
+++ app/src/main/java/com/example/user/navigationdrawersample/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.user.navigationdrawersample;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.Console;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeViews();
+        toggleDrawer();
+        initializeDefaultFragment(savedInstanceState,0);
+    }
+    private void initializeViews() {
+        toolbar = findViewById(R.id.toolbar_id);
+        toolbar.setTitle(R.string.toolbar_title);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout_id);
+        navigationView = findViewById(R.id.navigationview_id);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    private void initializeDefaultFragment(Bundle savedInstanceState, int itemIndex){
+        if (savedInstanceState == null){
+            MenuItem menuItem = navigationView.getMenu().getItem(itemIndex).setChecked(true);
+            onNavigationItemSelected(menuItem);
+        }
+    }
+    private void toggleDrawer() {
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+
+            case R.id.nav_chat_id:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id, new ChatFragment())
+                        .commit();
+                closeDrawer();
+                break;
+
+        }
+        return true;
+    }
+    private void closeDrawer(){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    private void deSelectCheckedState(){
+        int noOfItems = navigationView.getMenu().size();
+        for (int i=0; i<noOfItems;i++){
+            navigationView.getMenu().getItem(i).setChecked(false);
+        }
+    }
+}
